29778125c17104c2103a671158bf22671874bbf8
ibrahimasamake <115491419+ibrahimasamake@users.noreply.github.com>
2024-06-19 01:10:51 +0200

    refactore pos

--- app/pos/page.tsx
+++ app/pos/page.tsx
@@ -14,20 +14,15 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Card } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { formatPrice } from "@/lib/actionUtils";
+import { createInvoice, formatPrice } from "@/lib/actionUtils";
 import personnePhoto from "@/lib/image/3d/Personne.jpg";
-import {
-  eventSelectUseClient,
-  eventSelectUserProductPos,
-  eventSelectUserProductSelectPos,
-} from "@/lib/rxjsEvent";
-import jsPDF from "jspdf";
-import "jspdf-autotable";
-import { CircleX, Minus, Plus } from "lucide-react";
+import { eventSelectUseClient } from "@/lib/rxjsEvent";
+
 import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Subscription } from "rxjs";
+import { ListeSelect } from "./ListeSelectProducts";
+import { TotalPrixProductComponent } from "./TotalPriceProducts";
 import { AllProduitsSelectValide } from "./vente";
 
 type Product = {
@@ -37,162 +32,10 @@ type Product = {
   prix: number;
   name: string;
 };
+
 type ProductPosPropos = {
-  setFinalProduits: (proudct: Product[]) => void;
+  setFinalProduits: (product: Product[]) => void;
 };
-
-export function ListeSelect({ setFinalProduits }: ProductPosPropos) {
-  const [listProduit, setListProduct] = useState<Product[]>([]);
-
-  useEffect(() => {
-    const handleProductUpdate = (updatedProduct: Product) => {
-      setListProduct((prevList) => {
-        const existingProductIndex = prevList.findIndex(
-          (product) => product.id === updatedProduct.id
-        );
-
-        if (existingProductIndex !== -1) {
-          const updatedList = [...prevList];
-          updatedList[existingProductIndex] = {
-            ...updatedList[existingProductIndex],
-            quantite: updatedList[existingProductIndex].quantite + 1,
-          };
-          eventSelectUserProductSelectPos.next(updatedList);
-
-          return updatedList;
-        } else {
-          return [...prevList, updatedProduct];
-        }
-      });
-    };
-
-    const subscription =
-      eventSelectUserProductPos.subscribe(handleProductUpdate);
-    eventSelectUserProductSelectPos.next(listProduit);
-    return () => subscription.unsubscribe();
-  }, []);
-
-  const handleIncrement = useCallback((index: number) => {
-    setListProduct((prevList) => {
-      const updatedList = [...prevList];
-      updatedList[index] = {
-        ...updatedList[index],
-        quantite: updatedList[index].quantite + 1,
-      };
-      eventSelectUserProductSelectPos.next(updatedList);
-      return updatedList;
-    });
-  }, []);
-
-  const handleDecrement = useCallback((index: number) => {
-    setListProduct((prevList) => {
-      const updatedList = [...prevList];
-      if (updatedList[index].quantite > 1) {
-        updatedList[index] = {
-          ...updatedList[index],
-          quantite: updatedList[index].quantite - 1,
-        };
-      }
-      eventSelectUserProductSelectPos.next(updatedList);
-      return updatedList;
-    });
-  }, []);
-
-  const handleInputChange = useCallback((index: number, newValue: number) => {
-    setListProduct((prevList) => {
-      const updatedList = [...prevList];
-      updatedList[index] = {
-        ...updatedList[index],
-        quantite: newValue,
-      };
-      eventSelectUserProductSelectPos.next(updatedList);
-      return updatedList;
-    });
-  }, []);
-
-  const deleteItem = useCallback((index: number) => {
-    setListProduct((prevList) => {
-      const updatedList = [...prevList];
-      updatedList.splice(index, 1);
-      eventSelectUserProductSelectPos.next(updatedList);
-      return updatedList;
-    });
-  }, []);
-
-  useEffect(() => {
-    const Liste = () => {
-      return listProduit;
-    };
-    const liste = Liste();
-    eventSelectUserProductSelectPos.next(liste);
-    setFinalProduits(liste); // Met à jour finalProduits ici
-  }, [listProduit, setFinalProduits]);
-
-  return (
-    <div className="relative flex flex-col gap-1 self-end justify-end px-1">
-      {listProduit.map((item, index) => (
-        <Card
-          key={index}
-          className="p-1 relative border rounded-md border-success-600/20 flex gap-2 hover:bg-accent/50"
-        >
-          <div className="relative w-14 h-14">
-            <Image
-              src={item.image}
-              width={100}
-              height={100}
-              alt={`Product ${index}`}
-              className="absolute rounded size-14"
-            />
-          </div>
-          <div className="space-y-0.5 flex flex-col justify-between">
-            <p className="text-md ms-4 font-normal">
-              {item.name.toLocaleLowerCase()}
-            </p>
-            <div className="flex ms-4 items-center gap-0">
-              <div
-                className="border p-2  rounded-none rounded-br rounded-tr"
-                onClick={() => handleDecrement(index)}
-              >
-                <Minus size={12} />
-              </div>
-
-              <div className="w-12  bg-white">
-                <Input
-                  className="py-0 px-1 h-7 rounded-none"
-                  type="number"
-                  value={item.quantite}
-                  min={1}
-                  size={4}
-                  onChange={(e) =>
-                    handleInputChange(index, Number(e.target.value))
-                  }
-                />
-              </div>
-
-              <div
-                className="p-2 border  rounded-none rounded-bl rounded-tl"
-                onClick={() => handleIncrement(index)}
-              >
-                <Plus size={12} />
-              </div>
-              <div className="ms-4 text-tiny">Qte: {item.quantite}</div>
-            </div>
-            <div className="text-xs text-white absolute bg-amber-600  rounded-t bottom-1 rounded-b right-1 px-2 py-1 ">
-              {formatPrice(item.prix)}
-            </div>
-          </div>
-          <div className="absolute top-1 right-1 ">
-            <CircleX
-              className="hover:text-red-600 "
-              strokeWidth={1}
-              onClick={() => deleteItem(index)}
-            />
-          </div>
-        </Card>
-      ))}
-    </div>
-  );
-}
 
 type Client = {
   nom: string;
@@ -203,57 +46,6 @@ type Client = {
 type prix = {
   value: number;
 };
-
-export function TotalPrixProductComponent() {
-  const [prixTotalList, setPrixTotalList] = useState<Product[]>([]);
-  const [prix, setPrix] = useState<number>(0);
-
-  useEffect(() => {
-    const handleProductUpdate = (updatedPrice: Product) => {
-      setPrixTotalList((prevList) => {
-        const existingProductIndex = prevList.findIndex(
-          (product) => product.id === updatedPrice.id
-        );
-        if (existingProductIndex !== -1) {
-          const updatedList = [...prevList];
-          updatedList[existingProductIndex] = updatedPrice;
-          return updatedList;
-        } else {
-          return [...prevList, updatedPrice];
-        }
-      });
-    };
-
-    const subscription: Subscription =
-      eventSelectUserProductPos.subscribe(handleProductUpdate);
-    return () => subscription.unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    const Liste = (liste: Product[]) => {
-      setPrixTotalList(liste);
-    };
-    const subscription: Subscription =
-      eventSelectUserProductSelectPos.subscribe(Liste);
-    return () => subscription.unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    const newPrix = prixTotalList.reduce(
-      (accumulator, currentValue) =>
-        accumulator + currentValue.prix * currentValue.quantite,
-      0
-    );
-    setPrix(newPrix);
-  }, [prixTotalList]);
-
-  return (
-    <div className="flex-auto items-center px-4 bg-success-600/10 flex">
-      <div>TOTAL A PAYER</div>
-      <div className="ms-auto">{formatPrice(prix)} </div>
-    </div>
-  );
-}
 
 type ClientPos = {
   nom: string;
@@ -279,82 +71,6 @@ export default function Page() {
 
     return () => subscription.unsubscribe();
   }, []);
-
-  // Fonction pour créer une facture
-
-  function createInvoice() {
-    const doc = new jsPDF();
-
-    // Définir les styles globaux
-    doc.setFont("Helvetica", "bold");
-    doc.setFontSize(16);
-    doc.setTextColor(40, 40, 40);
-    doc.text("SAM INFORMATIQUE MALI", 14, 20);
-
-    // Ajouter le header avec les informations de l'entreprise
-    doc.setFontSize(12);
-    doc.setFont("Helvetica", "normal");
-    doc.text("Sam Informatique Mali", 14, 30);
-    doc.text("PDG: Ibrahima Samake", 14, 36);
-    doc.text("Numéro: 74171794", 14, 42);
-
-    // Générer la date et le numéro de la facture
-    const date = new Date().toLocaleDateString();
-    const invoiceNumber = Math.floor(Math.random() * 1000000).toString(); // Génère un numéro de facture unique
-
-    // Ajouter les informations de la facture
-    doc.setFontSize(12);
-    doc.setFont("Helvetica", "normal");
-    doc.text(`Date: ${date}`, 120, 20);
-    doc.text(`Facture N°: ${invoiceNumber}`, 120, 26);
-
-    // Ajouter les informations du client
-    if (client) {
-      doc.text(`Client: ${client.nom} ${client.prenom}`, 120, 36);
-      doc.text(`Numéro: ${client.numero}`, 120, 42);
-    } else {
-      doc.text("Client: inconnu", 120, 36);
-    }
-
-    // Ajouter les produits dans un tableau
-    const productsTable = finalProduits.map((product, index) => [
-      index + 1,
-      product.name,
-      product.quantite,
-      `${product.prix * product.quantite} `,
-    ]);
-
-    doc.autoTable({
-      head: [["#", "Produit", "Quantité", "Prix Total (FCFA)"]],
-      body: productsTable,
-      startY: 50,
-      theme: "grid",
-      headStyles: { fillColor: [22, 160, 133] }, // Couleur de l'en-tête
-      alternateRowStyles: { fillColor: [240, 240, 240] }, // Couleur des lignes alternées
-      styles: {
-        font: "Helvetica",
-        fontSize: 10,
-        textColor: [40, 40, 40],
-      },
-      margin: { top: 50 },
-    });
-
-    // Ajouter le total avec un fond moderne
-    const finalY = doc.previousAutoTable.finalY || 50;
-    doc.setFontSize(12);
-    doc.setTextColor(255, 255, 255); // Couleur du texte en blanc
-    doc.setFillColor(255, 98, 79); // Couleur de fond en rouge orangé
-    doc.rect(14, finalY + 8, 182, 12, "F"); // Rectangle pour le fond
-    doc.text(`Total = ${total} F CFA `, 120, finalY + 15); // Texte du total
-
-    // Ajouter un pied de page
-    doc.setFontSize(10);
-    doc.setTextColor(150, 150, 150);
-    doc.text("Merci pour votre achat!", 14, finalY + 25); // Texte du total
-
-    // Sauvegarder le PDF
-    doc.save("facture.pdf");
-  }
 
   return (
     <div className="flex flex-col lg:flex-row h-screen overflow-hidden">
@@ -442,8 +158,8 @@ export default function Page() {
                                     Genere la facture apres confirmation.
                                   </label>
                                   <p className="text-sm text-muted-foreground">
-                                    Veuller a alumer l'inprimente afin que la
-                                    facture de la vente soit bien imprimer.
+                                    Veuller a alumer l&aposinprimente afin que
+                                    la facture de la vente soit bien imprimer.
                                   </p>
                                 </div>
                               </div>
@@ -453,7 +169,11 @@ export default function Page() {
                       </AlertDialogHeader>
                       <AlertDialogFooter>
                         <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <AlertDialogAction onClick={createInvoice}>
+                        <AlertDialogAction
+                          onClick={() =>
+                            createInvoice(client, finalProduits, total)
+                          }
+                        >
                           Confirmer la vente
                         </AlertDialogAction>
                       </AlertDialogFooter>
--- lib/actionUtils.ts
+++ lib/actionUtils.ts
@@ -1,3 +1,6 @@
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
 export const formatPrice = (price: number) => {
   return price
     .toLocaleString("fr-FR", {
@@ -9,3 +12,94 @@ export const formatPrice = (price: number) => {
     .replace("FCFA", "FCFA")
     .trim(); // Retirer l'espace ajouté automatiquement
 };
+type ClientPos = {
+  nom: string;
+  prenom: string;
+  numero: string;
+};
+
+type Product = {
+  id: number;
+  quantite: number;
+  image: string;
+  prix: number;
+  name: string;
+};
+
+export function createInvoice(
+  client: ClientPos | null,
+  finalProduits: Product[],
+  total: number
+) {
+  const doc = new jsPDF();
+
+  // Définir les styles globaux
+  doc.setFont("Helvetica", "bold");
+  doc.setFontSize(16);
+  doc.setTextColor(40, 40, 40);
+  doc.text("SAM INFORMATIQUE MALI", 14, 20);
+
+  // Ajouter le header avec les informations de l'entreprise
+  doc.setFontSize(12);
+  doc.setFont("Helvetica", "normal");
+  doc.text("Sam Informatique Mali", 14, 30);
+  doc.text("PDG: Ibrahima Samake", 14, 36);
+  doc.text("Numéro: 74171794", 14, 42);
+
+  // Générer la date et le numéro de la facture
+  const date = new Date().toLocaleDateString();
+  const invoiceNumber = Math.floor(Math.random() * 1000000).toString(); // Génère un numéro de facture unique
+
+  // Ajouter les informations de la facture
+  doc.setFontSize(12);
+  doc.setFont("Helvetica", "normal");
+  doc.text(`Date: ${date}`, 120, 20);
+  doc.text(`Facture N°: ${invoiceNumber}`, 120, 26);
+
+  // Ajouter les informations du client
+  if (client) {
+    doc.text(`Client: ${client.nom} ${client.prenom}`, 120, 36);
+    doc.text(`Numéro: ${client.numero}`, 120, 42);
+  } else {
+    doc.text("Client: inconnu", 120, 36);
+  }
+
+  // Ajouter les produits dans un tableau
+  const productsTable = finalProduits.map((product, index) => [
+    index + 1,
+    product.name,
+    product.quantite,
+    `${product.prix * product.quantite} `,
+  ]);
+
+  doc.autoTable({
+    head: [["#", "Produit", "Quantité", "Prix Total (FCFA)"]],
+    body: productsTable,
+    startY: 50,
+    theme: "grid",
+    headStyles: { fillColor: [22, 160, 133] }, // Couleur de l'en-tête
+    alternateRowStyles: { fillColor: [240, 240, 240] }, // Couleur des lignes alternées
+    styles: {
+      font: "Helvetica",
+      fontSize: 10,
+      textColor: [40, 40, 40],
+    },
+    margin: { top: 50 },
+  });
+
+  // Ajouter le total avec un fond moderne
+  const finalY = doc.previousAutoTable.finalY || 50;
+  doc.setFontSize(12);
+  doc.setTextColor(255, 255, 255); // Couleur du texte en blanc
+  doc.setFillColor(255, 98, 79); // Couleur de fond en rouge orangé
+  doc.rect(14, finalY + 8, 182, 12, "F"); // Rectangle pour le fond
+  doc.text(`Total = ${total} F CFA `, 120, finalY + 15); // Texte du total
+
+  // Ajouter un pied de page
+  doc.setFontSize(10);
+  doc.setTextColor(150, 150, 150);
+  doc.text("Merci pour votre achat!", 14, finalY + 25); // Texte du total
+
+  // Sauvegarder le PDF
+  doc.save("facture.pdf");
+}
